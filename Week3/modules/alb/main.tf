451de4c6da9176c512f03c8a082aefd6f4c3c22b
abinshihab <abushihab_330@hotmail.com>
2025-08-12 23:13:00 +0400

    Update day18 Week3

--- Week3/modules/alb/main.tf
+++ Week3/modules/alb/main.tf
@@ -42,3 +42,17 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.this.arn
   }
 }
+
+resource "aws_lb_listener" "https" {
+  count             = length(var.acm_certificate_arn) > 0 ? 1 : 0
+  load_balancer_arn = aws_lb.this.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.acm_certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
--- Week3/modules/alb/variables.tf
+++ Week3/modules/alb/variables.tf
@@ -1,24 +1,31 @@
 variable "environment" {
+  description = "Deployment environment (e.g., dev, prod)"
   type        = string
-  description = "Environment name (e.g., dev, prod)"
 }
 
 variable "vpc_id" {
+  description = "VPC ID"
   type        = string
-  description = "VPC ID for the ALB and Target Group"
 }
 
 variable "subnet_ids" {
+  description = "List of subnet IDs for the ALB"
   type        = list(string)
-  description = "List of subnet IDs for ALB"
 }
 
 variable "security_group_ids" {
+  description = "List of security group IDs for the ALB"
   type        = list(string)
-  description = "List of security group IDs for ALB"
+}
+
+variable "acm_certificate_arn" {
+  description = "ACM Certificate ARN for HTTPS listener. Leave empty to disable HTTPS"
+  type        = string
+  default     = ""
 }
 
 variable "tags" {
-  type        = map(string)
   description = "Tags to apply"
+  type        = map(string)
+  default     = {}
 }
