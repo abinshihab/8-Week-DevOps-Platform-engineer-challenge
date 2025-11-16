01590f2aa216a8c229f67755dc408f3f7f851351
abinshihab <abushihab_330@hotmail.com>
2025-11-16 14:41:59 +0400

    Update day 45

--- Week6/backend.tf
+++ Week6/backend.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "s3" {
     # Your S3 bucket for storing Terraform state
-    #bucket = "cc8weeks-terraform-state"
+    bucket = "cc8weeks-terraform-state"
 
     # Environment-specific state path (keeps dev/stage/prod isolated)
-    #key    = "state/${var.env}/terraform.tfstate"
+    key    = "state/${var.env}/terraform.tfstate"
 
     # AWS region of your bucket
-    #region = "us-east-1"
+    region = "us-east-1"
 
     # ⚙️ Optional cost-related settings
     # encrypt        = true             # Disable for lab use to avoid unnecessary features
