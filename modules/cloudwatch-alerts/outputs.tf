6fec41f69b2b2ac2e23d1b7eb208b27b2184f256
abinshihab <abushihab_330@hotmail.com>
2025-11-20 03:02:05 +0400

    Update outputs for week 7

--- modules/cloudwatch-alerts/outputs.tf
+++ modules/cloudwatch-alerts/outputs.tf
@@ -7,13 +7,10 @@ output "sns_topic_arn" {
   value       = aws_sns_topic.alerts.arn
 }
 
+# Week 7: No ASG CPU alarm exists → output is always null.
 output "asg_cpu_alarm_name" {
-  description = "Name of the ASG CPU alarm"
-  value       = (
-    length(aws_cloudwatch_metric_alarm.asg_cpu_high) > 0 ?
-    aws_cloudwatch_metric_alarm.asg_cpu_high[0].alarm_name :
-    null
-  )
+  description = "ASG CPU alarm name (not created in Week 7)"
+  value       = null
 }
 
 output "alb_unhealthy_alarm_name" {
@@ -21,20 +18,13 @@ output "alb_unhealthy_alarm_name" {
   value       = aws_cloudwatch_metric_alarm.alb_unhealthy_hosts.alarm_name
 }
 
+# Week 7: Scaling is disabled → Never created → Must be null
 output "scale_out_policy_arn" {
-  description = "ARN of the Auto Scaling scale-out policy"
-  value       = (
-    length(aws_autoscaling_policy.scale_out) > 0 ?
-    aws_autoscaling_policy.scale_out[0].arn :
-    null
-  )
+  description = "Scale-out policy (disabled in Week 7)"
+  value       = null
 }
 
 output "scale_in_policy_arn" {
-  description = "ARN of the Auto Scaling scale-in policy"
-  value       = (
-    length(aws_autoscaling_policy.scale_in) > 0 ?
-    aws_autoscaling_policy.scale_in[0].arn :
-    null
-  )
+  description = "Scale-in policy (disabled in Week 7)"
+  value       = null
 }
